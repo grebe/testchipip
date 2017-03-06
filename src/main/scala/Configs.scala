710c5fa3db1f8cb528af1b33b07d11966853e5f2
Howard Mao <zhehao.mao@gmail.com>
2017-03-05 19:08:25 -0800

    add WithSRAM configuration

--- src/main/scala/Configs.scala
+++ src/main/scala/Configs.scala
@@ -2,8 +2,9 @@ package testchipip
 
 import chisel3._
 import unittest.UnitTests
-import rocketchip.{BaseConfig, NCoreplexExtClients}
-import uncore.tilelink.TLId
+import rocketchip.{BaseConfig, NCoreplexExtClients, ExtMemSize}
+import coreplex.NMemoryChannels
+import uncore.tilelink.{TLId, TLKey}
 import cde.{Parameters, Config, CDEMatchError}
 
 class WithTestChipUnitTests extends Config(
@@ -21,5 +22,16 @@ class TestChipUnitTestConfig extends Config(
 class WithSerialAdapter extends Config(
   (pname, site, here) => pname match {
     case SerialInterfaceWidth => 32
+    case _ => throw new CDEMatchError
+  })
+
+class WithSRAM(nBanksPerChannel: Int) extends Config(
+  (pname, site, here) => pname match {
+    case NSRAMBanksPerChannel => nBanksPerChannel
+    case NSRAMBlocksPerBank => {
+      val blockBytes = site(TLKey("L2toMC")).dataBits / 8
+      val nBanks = nBanksPerChannel * site(NMemoryChannels)
+      (site(ExtMemSize) / (nBanks * blockBytes)).toInt
+    }
     case _ => throw new CDEMatchError
   })
